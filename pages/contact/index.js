c9dfd9dae367046a8e614cf96473bf82979ca193
sebinsaju <65224759+sebinsaju@users.noreply.github.com>
2022-06-27 22:50:14 +0530

    fixes

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -3,24 +3,24 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div><section class="w3l-breadcrumb">
-            <div class="breadcrum-bg py-sm-5 py-4">
-                <div class="container py-lg-3">
+        <div><section className="w3l-breadcrumb">
+            <div className="breadcrum-bg py-sm-5 py-4">
+                <div className="container py-lg-3">
                     <h2>Contact Us</h2>
                     <p><Link href="/"><a >Home</a></Link> &nbsp; / &nbsp; Contact</p>
 
                 </div>
             </div>
         </section>
-            <section class="w3l-contact-1 py-5">
-                <div class="contacts-9 py-lg-5 py-sm-4">
-                    <div class="container">
-                        <div class=" contact-view">
-                            <div class="cont-details">
+            <section className="w3l-contact-1 py-5">
+                <div className="contacts-9 py-lg-5 py-sm-4">
+                    <div className="container">
+                        <div className=" contact-view">
+                            <div className="cont-details">
                                 {/* <p>Get in touch</p> */}
-                                <h3 class="title-big">Contact Radiant Resort near Bangalore</h3>
+                                <h3 className="title-big">Contact Radiant Resort near Bangalore</h3>
                             </div>
-                            {/* <div class="map-content-9">
+                            {/* <div className="map-content-9">
                                 <p>Radiant Resort<br />
                                     CK Palya Road, 17th km,
                                     Bannerghatta Road,
@@ -32,39 +32,39 @@ const Contact = () => {
                                     Email:info@radiantresort.com</p>
                             </div> */}
                         </div>
-                        <div class="map-iframe my-5">
+                        <div className="map-iframe my-5">
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3890.02522436737!2d77.58822121453278!3d12.841646490941235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baef97510555555%3A0x9d59a83e59e2d57f!2sRadiant%20Resort!5e0!3m2!1sen!2sin!4v1656229458009!5m2!1sen!2sin"
-                                width="100%" height="300" frameborder="0" style={{ border: "0px", pointerEvents: "none" }}
-                                allowfullscreen=""></iframe>
+                                width="100%" height="300" frameBorder="0" style={{ border: "0px", pointerEvents: "none" }}
+                                allowFullScreen=""></iframe>
                         </div>
-                        <div class="d-grid contact-view">
-                            <div class="cont-details">
-                                <div class="cont-top">
-                                    <div class="cont-left text-center">
-                                        <span class="fa fa-phone text-primary"></span>
+                        <div className="d-grid contact-view">
+                            <div className="cont-details">
+                                <div className="cont-top">
+                                    <div className="cont-left text-center">
+                                        <span className="fa fa-phone text-primary"></span>
                                     </div>
-                                    <div class="cont-right">
+                                    <div className="cont-right">
                                         <h6>Call Us</h6>
                                         <p>+91809598 8877 | Resort Front Desk and Reception<br/>
                                             +91808888 7777 | Corporate Sales and Enquiry<br/>
                                             +91 80 5002 7359 | Ayurvedic Spa and Facilities</p>
                                     </div>
                                 </div>
-                                <div class="cont-top margin-up">
-                                    <div class="cont-left text-center">
-                                        <span class="fa fa-envelope-o text-primary"></span>
+                                <div className="cont-top margin-up">
+                                    <div className="cont-left text-center">
+                                        <span className="fa fa-envelope-o text-primary"></span>
                                     </div>
-                                    <div class="cont-right">
+                                    <div className="cont-right">
                                         <h6>Email Us</h6>
-                                        <p><Link href="mailto:example@mail.com"><a class="mail">info@radiantresort.com</a></Link></p>
+                                        <p><Link href="mailto:example@mail.com"><a className="mail">info@radiantresort.com</a></Link></p>
                                     </div>
                                 </div>
-                                <div class="cont-top margin-up">
-                                    <div class="cont-left text-center">
-                                        <span class="fa fa-map-marker text-primary"></span>
+                                <div className="cont-top margin-up">
+                                    <div className="cont-left text-center">
+                                        <span className="fa fa-map-marker text-primary"></span>
                                     </div>
-                                    <div class="cont-right">
+                                    <div className="cont-right">
                                         <h6>Address</h6>
                                         <p>Radiant Resort<br />
                                     CK Palya Road, 17th km,
@@ -73,21 +73,21 @@ const Contact = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div class="map-content-9 mt-lg-0 mt-4">
+                            <div className="map-content-9 mt-lg-0 mt-4">
                                 <form action="https://sendmail.w3layouts.com/submitForm" method="post">
-                                    <div class="twice-two">
-                                        <input type="text" class="form-control" name="w3lName" id="w3lName" placeholder="Name"
+                                    <div className="twice-two">
+                                        <input type="text" className="form-control" name="w3lName" id="w3lName" placeholder="Name"
                                             required="" />
-                                        <input type="email" class="form-control" name="w3lSender" id="w3lSender" placeholder="Email"
+                                        <input type="email" className="form-control" name="w3lSender" id="w3lSender" placeholder="Email"
                                             required="" />
                                     </div>
-                                    <div class="twice">
-                                        <input type="text" class="form-control" name="w3lSubject" id="w3lSubject"
+                                    <div className="twice">
+                                        <input type="text" className="form-control" name="w3lSubject" id="w3lSubject"
                                             placeholder="Subject" required="" />
                                     </div>
-                                    <textarea name="w3lMessage" class="form-control" id="w3lMessage" placeholder="Message"
+                                    <textarea name="w3lMessage" className="form-control" id="w3lMessage" placeholder="Message"
                                         required=""></textarea>
-                                    <button type="submit" class="btn btn-contact">Send Message</button>
+                                    <button type="submit" className="btn btn-contact">Send Message</button>
                                 </form>
                             </div>
                         </div>
---
+++ pages/gallery/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Gallery = () => {
+    const IMAGES = [{ image: "/gallery/1.jpg" }, { image: "/gallery/2.jpg" }, { image: "/gallery/3.jpg" }, { image: "/gallery/4.jpg" }, { image: "/gallery/5.jpg" }, { image: "/gallery/6.jpg" }, { image: "/gallery/7.jpg" }, { image: "/gallery/8.jpg" }, { image: "/gallery/9.jpg" }, { image: "/gallery/10.jpg" }, { image: "/gallery/11.jpg" }, { image: "/gallery/12.jpg" }, { image: "/gallery/13.jpg" }, { image: "/gallery/14.jpg" }, { image: "/gallery/15.jpg" }, { image: "/gallery/16.jpg" }, { image: "/gallery/17.jpg" }, { image: "/gallery/18.jpg" }, { image: "/gallery/19.jpg" }, { image: "/gallery/20.jpg" }, { image: "/gallery/21.jpg" }, { image: "/gallery/22.jpg" }, { image: "/gallery/23.jpg" }, { image: "/gallery/24.jpg" }, { image: "/gallery/25.jpg" }, { image: "/gallery/26.jpg" }]
+    return (
+        <div></div>
+    )
+}
+
+export default Gallery
