292eef57f23c1879e05860b5f9c084740e8c7eca
sebin <sebin@fegno.com>
2022-08-11 17:32:58 +0530

    fix

--- axios/index.js
+++ axios/index.js
@@ -2,8 +2,4 @@ import axios from "axios";
 const baseURL = 'http://api-radiant.herokuapp.com/';
 export const instance = axios.create({
     baseURL: baseURL,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      'Content-Type': 'application/json',
-    }
 })
