861e7d145478b96743da926114605924693de252
sebin <sebin@fegno.com>
2022-12-28 21:32:48 +0530

    https changed

--- axios/index.js
+++ axios/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = 'http://3.109.182.157:4000/api/v1/';
+const baseURL = 'https://3.109.182.157:4000/api/v1/';
 export const instance = axios.create({
     baseURL: baseURL,
     headers: {
