373f9315f7668bc3fa0c32835a809bd6dd72174a
sebin <sebin@fegno.com>
2023-01-07 21:40:08 +0530

    link changed

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,1 @@
-export const bookingLink =
-  "https://app.axisrooms.com/beV2/displaySearchResultV3.html?applicableDealId=0&room_number=0&login=false&allHotels=true&searcherId=47179446&searchNumber=1";
+export const bookingLink ="https://app.axisrooms.com/beV2/searchHotel.html?paxInfo=2%7C0%7C%7C&allHotels=true&newBe=true&productId=52995&bookingEngineId=1273&rooms=1&searchId=-1&searchNumber=1";
